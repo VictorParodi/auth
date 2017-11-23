ff6d782d84ca14204f0946885a2df0378d949c70
Victor <victor.parodi.montero@gmail.com>
2017-11-22 23:31:28 -0500

    rama1 commit 4
    Utilizando el componente TextInput y dándole estilos básicos.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { TextInput } from 'react-native';
 import { Card, CardSection, Button } from './common';
 
 class LoginForm extends Component {
     render() {
         return(
             <Card>
-                <CardSection />
+                <CardSection>
+                    <TextInput style={{height: 30, width: 100}} underlineColorAndroid="rgba(0,0,0,0)" />
+                </CardSection>
+
                 <CardSection />
 
                 <CardSection>
