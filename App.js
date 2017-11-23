7fc8bf698e9264b4a48f177de7261d6ff3131817
Victor <victor.parodi.montero@gmail.com>
2017-11-22 22:45:37 -0500

    rama1 commit 2
    Instalando firebase. Inicializando firebase en la aplicación.

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './src/components/common';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyDy9ufVSPRkqny0uBvNQ_1E1i18eY3G1C8",
+      authDomain: "auth-59067.firebaseapp.com",
+      databaseURL: "https://auth-59067.firebaseio.com",
+      projectId: "auth-59067",
+      storageBucket: "auth-59067.appspot.com",
+      messagingSenderId: "733856123801"
+    });
+  }
+
   render() {
     return (
       <View>
